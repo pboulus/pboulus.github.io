fc5259a8362ea9937a0573ba9d2da5fd89dd484f
Paul Boulus <boulus@pjpl.com.au>
2017-09-05 14:30:46 +1000

    Update to explicit Python3 call

--- gay-marriage-pulse/app/update.sh
+++ gay-marriage-pulse/app/update.sh
@@ -1,6 +1,6 @@
 cd ~/pboulus.github.io/gay-marriage-pulse/app/
 git pull origin master
-python update.py
+python3 update.py
 git add -A 
 git commit -m "Automatic data update"
 git push origin master
